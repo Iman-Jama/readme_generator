4f5d306c859067f76977ccbd736075a009c1065f
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-03-26 18:24:57 +0100

    Updated index.js file, now generates basic README file

---
+++ Develop/README.md
@@ -0,0 +1,41 @@
+# IMans README generator:
+
+## Description:
+I have created a simple readme generator using inquirer      and node JS
+
+
+## Table of Contents:
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+Your need to have VSCODE
+
+
+## Usage
+Open termianl in VSCODE and run the program using bash
+
+    
+    ![alt text](assets/images/not required herer.png)
+    
+
+## Credits
+
+The following have collaborated with me on my project:
+- [Iman-Jama](www.github.com/Iman-Jama)
+
+
+## License
+MIT
+
+
+## How to Contribute
+
+
+
+## Contact me:
+My github is: Iman-Jama
+My email is : iman.jama891@gmail.com
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,110 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+const generateReadMe = ({ title, Description, installation, Usage, screenshot, Collaborators, License, Github, email }) =>
+`# ${title}:
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description:
+${Description}
 
-// Function call to initialize app
-init();
+
+## Table of Contents:
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+${installation}
+
+
+## Usage
+${Usage}
+
+    
+    ![alt text](assets/images/${screenshot}.png)
+    
+
+## Credits
+
+The following have collaborated with me on my project:
+- [${Collaborators}](www.github.com/${Collaborators})
+
+
+## License
+${License}
+
+
+## How to Contribute
+
+
+
+## Contact me:
+My github is: ${Github}
+My email is : ${email}`;
+
+    inquirer
+      .prompt([
+        {
+            type:'input',
+            message:'Please enter the title of your application',
+            name:'title',
+        },
+        {
+          type: 'input',
+          message: 'Please provide a short description explaining the what, why and how of your application',
+          name: 'Description',
+        },
+        {
+          type: 'input',
+          message: 'What are the steps required to install your project?',
+          name: 'installation',
+        },
+        {
+          type: 'input',
+          message: 'Provide instructions and examples for use',
+          name: 'Usage',
+        },
+        {
+            type:'input',
+            message:'Please save your screenshots in an assets/images files in your repository. Now enter the name of your screenshot file.',
+            name:'screenshot',
+        },
+        {
+            type: 'input',
+            message:'List your collaborators, if any, with links to their GitHub profiles.',
+            name:'Collaborators',
+        },
+        {
+            type:'input',
+            message:'Please choose from the following list of licenses',
+            name:'License',
+        },
+        {
+            type:'input',
+            message:'Please enter your github username',
+            name:'Github',
+        },
+        {
+            type:'input',
+            message:'Please enter your email address',
+            name:'email',
+        },
+      ])
+
+.then((answers) =>{
+    const ReadMeContent = generateReadMe(answers);
+
+    fs.writeFile('README.md', ReadMeContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created ReadMe file!')
+    );
+  });
+
+
